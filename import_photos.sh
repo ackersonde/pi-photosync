af60a525ad2e870cb2a5b33434ef3b66493df7af
Dan Ackerson <dan@ackerson.de>
2022-02-06 16:21:04 +0100

    need interactive flag

--- import_photos.sh
+++ import_photos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # TODO if syncthing is Up to Date (not syncing!)
 # then
-docker-compose -f /home/ubuntu/photoprism/docker-compose.yml exec photoprism photoprism import
+docker-compose -f /home/ubuntu/photoprism/docker-compose.yml exec -i photoprism photoprism import
