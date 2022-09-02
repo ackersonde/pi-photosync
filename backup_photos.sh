2251185d745a97c8cc0b2f4de501901d5cebccea
Dan Ackerson <dan@ackerson.de>
2022-09-02 10:17:02 +0200

    use ssh/config

--- backup_photos.sh
+++ backup_photos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -a /home/ubuntu/Pictures/ 192.168.178.28:/mnt/usb4TB/backups/photos/originals/
-rsync -a /home/ubuntu/Pictures/ root@vault.ackerson.de:/mnt/hetzner_disk/backups/photos/
+rsync -a /home/ubuntu/Pictures/ vault:/mnt/hetzner_disk/backups/photos/
